d96db8f90f793d8a4c81776d41ac0d991efafec4
2558001183-design <2558001183@utna.edu.mx>
2026-09-17 09:21:51 -0600

    Pasen videos candentes de josé

--- ADNN.java
+++ ADNN.java
@@ -1,3 +1,4 @@
 public class index {
-/* Hace un rato me picó un mosquito */
+/* Hace un rato me picó un mosquito y el culero me agarró porque andaba solito, 
+si hubiera habido más banda le damos en la madre, si lo vuelvo a ver le voy a decir que se cuadre.  */
 }
